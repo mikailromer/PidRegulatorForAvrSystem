d8d841d69e964571f8aa8aae1067880a40cc9bb3
Michal Romero <mcamromer@gmail.com>
2019-04-25 18:39:45 +0200

    Create simulation of AVR regulation system, using PID controller, without implementation of FA algorithm.

--- AvrSystem.py
+++ AvrSystem.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ElementTree
-from os import path, getcwd
+from control import tf,step_response
+from os import path, getcwd, mkdir
+import matplotlib.pyplot as plt
 
 def parseConfigurationFile():
     filePath=path.join(getcwd(), "config", "config.xml")
@@ -46,7 +48,42 @@ def parseConfigurationFile():
 
     return FaAlgorithm, amplifier, exciter, generator, sensor
 
+def step_info(t,yout):
+    overshoot =(yout.max()/yout[-1]-1)*100
+    risingTime = t[next(i for i in range(0,len(yout)-1) if yout[i]>yout[-1]*.90)]-t[0]
+    settingTime = t[next(len(yout)-i for i in range(2,len(yout)-1) if abs(yout[-i]/yout[-1])>1.02)]-t[0]
+    print("OS: %f%s"%(overshoot,'%'))
+    print("Tr: %fs"%(risingTime))
+    print("Ts: %fs"%(settingTime))
+
+    return overshoot, risingTime, settingTime
+
+
+
 if __name__ == '__main__':
 
-    FaAlgorithm, amp, ex, gen, sen =parseConfigurationFile()
-    print("DEBUG")
+    faAlgorithmParams, amplifierParams, exciterParams, generatorParams, sensorParams =parseConfigurationFile()
+    Kp = 1.3
+    Ki= 0.7
+    Kd = 0.1
+    voltageReference = 5
+    # Laplace's transform for elements of AVR system
+    Gpid = Kp + tf([Ki],[1,0]) + tf([Kd,0],[1])
+    Ga = tf([float(amplifierParams["Ka"])], [float(amplifierParams["Ta"]), 1])
+    Ge = tf([float(exciterParams["Ke"])], [float(exciterParams["Te"]), 1])
+    Gg = tf([float(generatorParams["Kg"])], [float(generatorParams["Tg"]), 1])
+    Gs = tf([float(sensorParams["Ks"])], [float(sensorParams["Ts"]), 1])
+
+    systemTransferFunction= (Gpid *Ga * Ge * Gg)/(1 + (Gpid *Ga * Ge * Gg))
+    T, yout= step_response(systemTransferFunction*voltageReference)
+    M, Tr, Ts = step_info(T,yout)
+    plt.plot(T,yout)
+    plt.title("AVR Regulation System's Response")
+    plt.xlabel("Time [s]")
+    plt.xlabel("Output Voltage [V]")
+    plt.xlim((0,T[len(T)-1]))
+    plt.grid()
+    plt.show()
+
+
+
